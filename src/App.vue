5089ddb19dc00b1f9ba4658a9cb87d0f765b3200
saragenuardi <saretta.summer16@gmail.com>
2022-06-01 17:23:05 +0200

    Aggiunto Header, da completare

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,86 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <AppHeader />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import AppHeader from "./components/AppHeader.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    AppHeader,
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
 }
+
+.container{
+  width: 1200px;
+  margin: 0 auto;
+}
+
+ul {
+  list-style-type: none;
+}
+
+.clearfix::after{
+  content: "";
+  display: table;
+  clear: both;
+}
+
+.fl{
+  float: left;
+}
+
+header{
+  background-color: #0a1750;
+  height: 680px;
+  background-image: url("./assets/images/header.png");
+  background-repeat: no-repeat;
+  background-size: 40%;
+  background-position: 80% 60%;
+}
+
+header .jumbotron h1 {
+  width: 380px;
+  margin-top: 100px;
+  color: #fff;
+  font-size: 45px;
+}
+header .jumbotron span {
+  font-weight: normal;
+  margin: 5px 0;
+}
+header .jumbotron p {
+  margin-top: 30px;
+  width: 400px;
+  color: #03a7d3e6;
+}
+
+.services {
+  text-align: center;
+}
+.services-list {
+  display: flex;
+  justify-content: space-between;
+} 
+
+button {
+  background: linear-gradient(to right, #ff5db1 0%, #ef017c 100%);
+  border-radius: 25px;
+  width: 120px;
+  height: 40px;
+  border: none;
+  color: #fff;
+}
+
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <header>
+    <div class="container">
+      <div class="navbar">
+        <div class="navbar-left">
+          <img src="images/white-logo-2.png" alt="" />
+        </div>
+        <div class="navbar-right">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="jumbotron">
+        <h1>
+          Businnes Planning
+          <span>Digital Technology</span>
+          Modern Solution!
+        </h1>
+        <p>
+          When, while the lovely valley teems witch vapour around meand the
+          meridian sun strikes the upper surface.
+        </p>
+        <button class="pink-button">Try It Now</button>
+        <button class="blue-button">Explore More</button>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
