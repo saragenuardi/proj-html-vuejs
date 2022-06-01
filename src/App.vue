4b1c9abd5cd0d420fef1f8caeb8f5a9dc62db653
saragenuardi <saretta.summer16@gmail.com>
2022-06-01 18:11:55 +0200

    modificato app header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-  <AppHeader />
+    <AppHeader />
   </div>
 </template>
 
@@ -16,65 +16,68 @@ export default {
 </script>
 
 <style lang="scss">
-*{
+//VARIABILI
+
+//Colori
+$blue-background: #0a1750;
+
+//Fonts
+
+$font-body: 'Open Sans', sans-serif;
+$font-title: 'Heebo', sans-serif; 
+//RESET
+* {
   box-sizing: border-box;
   padding: 0;
   margin: 0;
 }
 
-.container{
+
+@mixin buttons-style {
+  border-radius: 25px;
+  width: 140px;
+  height: 40px;
+  border: none;
+  color: #fff;
+}
+@mixin flex-space-between {
+  display: flex;
+  justify-content: space-between;
+}
+
+//COMMON
+body {
+  font-family: $font-body;
+}
+.container {
   width: 1200px;
   margin: 0 auto;
+}
+
+h1, h2, h3{
+  font-family: $font-title;
+}
+
+.weight-normal{
+  font-weight: normal;
 }
 
 ul {
   list-style-type: none;
 }
 
-.clearfix::after{
+.clearfix::after {
   content: "";
   display: table;
   clear: both;
 }
 
-.fl{
+.fl {
   float: left;
 }
 
-header{
-  background-color: #0a1750;
-  height: 680px;
-  background-image: url("./assets/images/header.png");
-  background-repeat: no-repeat;
-  background-size: 40%;
-  background-position: 80% 60%;
-}
-
-header .jumbotron h1 {
-  width: 380px;
-  margin-top: 100px;
-  color: #fff;
-  font-size: 45px;
-}
-header .jumbotron span {
-  font-weight: normal;
-  margin: 5px 0;
-}
-header .jumbotron p {
-  margin-top: 30px;
-  width: 400px;
-  color: #03a7d3e6;
-}
-
-.services {
-  text-align: center;
-}
-.services-list {
-  display: flex;
-  justify-content: space-between;
-} 
-
 button {
+  //Utilizzo gradient per la sfumatura
   background: linear-gradient(to right, #ff5db1 0%, #ef017c 100%);
   border-radius: 25px;
   width: 120px;
@@ -83,4 +86,42 @@ button {
   color: #fff;
 }
 
+//Header
+header {
+  background-color: $blue-background;
+  height: 680px;
+  background-image: url("./assets/images/header.png");
+  background-repeat: no-repeat;
+  background-size: 40%;
+  background-position: 80% 60%;
+
+  .jumbotron {
+    h1 {
+      width: 380px;
+      margin-top: 100px;
+      color: #fff;
+      font-size: 45px;
+    }
+
+    span {
+      font-weight: normal;
+      margin: 5px 0;
+    }
+
+    p {
+      margin-top: 30px;
+      width: 400px;
+      color: #03a7d3e6;
+    }
+  }
+}
+
+.services {
+  text-align: center;
+
+  &-list {
+    display: flex;
+    justify-content: space-between;
+  }
+}
 </style>
